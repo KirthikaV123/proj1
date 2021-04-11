f6bf6c16d16827b8b7305eb67cf7eb92d3c1952a
KirthikaV123 <82315597+KirthikaV123@users.noreply.github.com>
2021-04-11 16:28:18 +0530

    Delete includes directory

--- includes/dbh.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername ="localhost";
-$dbUsername = "root";
-$dbPassword =""; 
-$dbName = "customer details";
-
-$conn= mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
