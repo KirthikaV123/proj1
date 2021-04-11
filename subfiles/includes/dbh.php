0f2b3c433c1779b50cb85a40c7a300e19cc52e93
KirthikaV123 <82315597+KirthikaV123@users.noreply.github.com>
2021-04-11 16:29:52 +0530

    Add files via upload

---
+++ subfiles/includes/dbh.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername ="localhost";
+$dbUsername = "root";
+$dbPassword =""; 
+$dbName = "customer details";
+
+$conn= mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
---
+++ subfiles/index1.php
@@ -0,0 +1,15 @@
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home Page</title>
+</head>
+<body>
+  <h1>Transaction Services</h1>
+
+  <a href="https://localhost/customerdetails.php"><button type="button">Customer Details</button></a>
+  <a href="https://localhost/transaction.php"><button type="button">Start Transaction</button></a>
+</body>
+</html>
